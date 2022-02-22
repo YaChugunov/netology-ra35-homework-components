65285f4ed6ed01ac5a857909d9d1ecce2ff9d1bd
YaChugunov <informed.looser@gmail.com>
2022-02-23 00:00:54 +0300

    1 file modified

--- src/classes/classItem.js
+++ src/classes/classItem.js
@@ -1,12 +1,25 @@
-class ItemShop {
-  constructor ( id, name, status, online, avatar, interests ) { 
-    this.id = id;
-    this.name = name;
-    this.status = status;
-    this.online = online;
-    this.avatar = avatar;
-    this.interests = interests;
+class ShopItemFunc extends React.Component {
+  constructor(
+    brand,
+    title,
+    description,
+    descriptionFull,
+    price,
+    currency,
+    button
+  ) {
+    this.brand = brand;
+    this.title = title;
+    this.description = description;
+    this.descriptionFull = descriptionFull;
+    this.price = price;
+    this.currency = currency;
+    this.button = button;
+  }
+
+  render() {
+    <div></div>;
   }
 }
 
-export default ItemShop;
+export default ShopItemFunc;
